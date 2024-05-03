7a8c2d87c45499d51d0a7011064bc218640a4116
Arman Radmanesh <Radmanesh@gmail.com>
2024-05-03 00:17:46 -0500

    chore: Add PDF viewer component for displaying resume

---
+++ src/components/PDFContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
+const PDFContainer = ({ fileUrl }) => {
+    return (
+        <div style={{ height: '750px' }}>
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+                <Viewer fileUrl={fileUrl} />
+            </Worker>
+        </div>
+    );
+};
+
+export default PDFContainer;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,13 +1,14 @@
 import { Image } from '@mui/icons-material';
 import { Typography } from '@mui/material';
 import React from 'react';
+import PDFContainer from '../components/PDFContainer';
 export default function Resume() {
   return (
     <>
       <Typography variant="h4" color="text.secondary" align="center" > Resume </Typography>
-      <Image src='/cv1.png'  />
-      <Image src='/cv2.png'  />
-      <Image src='/cv3.png'  />
+      <PDFContainer fileUrl="/cv.pdf" />
+      {/* <Image src='/cv1.png'  />
+      <Image src='/cv2.png' /> */}
     </>
   );
 }
