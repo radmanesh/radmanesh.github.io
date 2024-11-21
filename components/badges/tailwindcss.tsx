74d381b5fe63b89bc13de1aadc699bf80b88472d
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-22 01:33:24 +0200

    feat (badges): add badge component for react, next, typescript & tailwind

---
+++ components/badges/tailwindcss.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "../ui/badge";
+
+export function TailwindCssBadge() {
+  return (
+    <Badge
+      variant="outline"
+      className="rounded-full border border-[#06B6D4] bg-[#06B6D4]/10 text-[#06B6D4]"
+    >
+      Tailwind CSS
+    </Badge>
+  );
+}
