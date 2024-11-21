ab8998e4e79075238c82b58eac87a63b7ab17fbd
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-22 01:33:46 +0200

    refactor (header): remove job title

--- components/header.tsx
+++ components/header.tsx
@@ -9,12 +9,7 @@ export default function Header() {
       <div className="px-4 sm:px-6">
         <div className="mx-auto mb-16 flex h-[72px] w-full max-w-3xl items-center justify-between border-b border-border/70">
           <Link href="/" aria-label="Home">
-            <h1 className="font-mono hover:opacity-50 font-semibold">
-              Som3aware
-            </h1>
-            <p className="font-mono text-xs opacity-60 hover:opacity-30">
-              Software Engineer
-            </p>
+            <h1 className="font-mono hover:opacity-50 font-bold">Som3aware</h1>
           </Link>
           <nav>
             <ul className="flex items-center gap-4">
