2e086488a0f37c7cac18f1384fd875e927d34241
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-22 21:44:10 +0200

    feat: add navigation content

---
+++ content/navigation.tsx
@@ -0,0 +1,9 @@
+import { NavigationItem } from "som3aware";
+
+const NAVIGATION_ITEMS = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Projects", href: "/projects" },
+] satisfies NavigationItem[];
+
+export default NAVIGATION_ITEMS;
