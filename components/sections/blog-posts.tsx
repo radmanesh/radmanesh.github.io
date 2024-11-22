e94c734db01b0018f14561e1f0624d573a0af492
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-22 03:00:28 +0200

    feat: start blog post section component

---
+++ components/sections/blog-posts.tsx
@@ -0,0 +1,10 @@
+export function BlogPosts() {
+    return (
+      <div className="space-y-8 tracking-tight font-light">
+        <h2 className="font-bold text-xl md:text-2xl underline decoration-wavy underline-offset-8">
+          Blog posts
+        </h2>
+      </div>
+    );
+  }
+  
