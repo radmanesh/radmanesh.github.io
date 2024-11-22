68413f3213cd2679ab602d72242c6e6c406bdf44
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-22 03:00:41 +0200

    feat: start featured projects section component

---
+++ components/sections/featured-projects.tsx
@@ -0,0 +1,9 @@
+export function FeaturedProjects() {
+  return (
+    <div className="space-y-8 tracking-tight font-light">
+      <h2 className="font-bold text-xl md:text-2xl underline decoration-wavy underline-offset-8">
+        Featured projects
+      </h2>
+    </div>
+  );
+}
