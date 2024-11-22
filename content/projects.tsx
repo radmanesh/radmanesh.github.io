853ee5482d3ab0d4274bf6c560a307f8d55de82e
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-22 21:43:57 +0200

    refactor: add more projects

--- content/projects.tsx
+++ content/projects.tsx
@@ -1,16 +1,31 @@
 import { Project } from "som3aware";
+import { Boxes, Hash, PencilRuler } from "lucide-react";
 
-const GITHUB_BASE_URL = "https://github.com";
-
-export default [
+const PROJECTS = [
   {
     id: "1",
+    featured: true,
     title: "Editor Setup",
+    url: "https://editorsetup.vercel.app/",
+    logo: <PencilRuler className="project-logo" />,
     description: "Find your next optimal VS Code Setup.",
-    codeLink: `${GITHUB_BASE_URL}/${process.env.GITHUB_USERNAME}/editor-setup`,
-    previewLink: "https://editor-setup.vercel.app",
-    logoPath: "/projects/editor-setup-logo.sv",
-    stackIconPath: "",
-    technologies: ["React", "TypeScript", "Tailwind CSS"],
+  },
+  {
+    id: "2",
+    featured: true,
+    title: "Sharp Studio",
+    url: "https://sharpstudio.vercel.app/",
+    logo: <Hash className="project-logo" />,
+    description: "Hackable Image Processing.",
+  },
+  {
+    id: "3",
+    featured: true,
+    title: "Open Trivia",
+    url: "https://open-trivia-demo.vercel.app/",
+    logo: <Boxes className="project-logo" />,
+    description: "Multi-round trivia game built with Open Trivia API.",
   },
 ] satisfies Project[];
+
+export default PROJECTS;
