13ea4afe68e902d969c93a567debdf5d81248419
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-22 02:59:47 +0200

    style: add selection color

--- components/badges/react.tsx
+++ components/badges/react.tsx
@@ -4,7 +4,7 @@ export function ReactBadge() {
   return (
     <Badge
       variant="outline"
-      className="rounded-full border border-[#61DAFB] bg-[#61DAFB]/10 text-[#61DAFB]"
+      className="rounded-full border border-[#61DAFB] bg-[#61DAFB]/10 text-[#61DAFB] selection:text-[#61DAFB]/80"
     >
       React
     </Badge>
