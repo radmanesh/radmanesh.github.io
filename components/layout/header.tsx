af14bf8ac74dea0ab7ea93ed08a96e7e5bb93a65
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-22 02:59:35 +0200

    refactor: change the height to h-16

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -8,7 +8,7 @@ export function Header() {
   return (
     <header>
       <div className="px-4 sm:px-6">
-        <div className="mx-auto mb-16 flex h-[72px] w-full max-w-3xl items-center justify-between border-b border-border/70">
+        <div className="mx-auto mb-16 flex h-16 w-full max-w-3xl items-center justify-between border-b border-border/70">
           <Link href="/" aria-label="Home">
             <h1 className="font-mono hover:opacity-50 font-bold">Som3aware</h1>
           </Link>
