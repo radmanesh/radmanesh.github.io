f6eb62b53b90977e7ba3fb79f1446fd03bc1f3a7
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-23 23:22:36 +0200

    feat: complete projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,38 @@
-export default function ProjectsPage() {
-  return <h1>Coming soon...</h1>;
+import PROJECTS from "@/content/projects";
+import { SectionTitle } from "@/components/shared/section";
+import { ProjectCard } from "@/components/shared/project-card";
+import { PaginationButton } from "@/components/shared/pagination-button";
+
+// --------- PAGE PROPS---------
+interface ProjectsPageProps {
+  searchParams: { page?: string };
+}
+
+// --------- PAGE ---------
+export default function ProjectsPage({ searchParams }: ProjectsPageProps) {
+  const start = 0;
+  const ITEMS_PER_PAGE = 6;
+  const currentPage = Number(searchParams.page) || 1;
+  const end = ITEMS_PER_PAGE * currentPage;
+
+  const visibleProjects = PROJECTS.slice(start, end);
+  const hasMore = end < PROJECTS.length;
+
+  return (
+    <div className="space-y-12">
+      <SectionTitle text="Projects" />
+      <div className="grid grid-cols-1 gap-4">
+        {visibleProjects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <PaginationButton
+          hasMore={hasMore}
+          baseHref="/projects"
+          currentPage={currentPage}
+        />
+      </div>
+    </div>
+  );
 }
