b5ae7dfdbb3ac80edda4fa423c1f45120608fbd2
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-26 18:16:10 +0200

    refactor: change tags to tag & add readingTime to computed fields

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,24 +1,31 @@
 // file: contentlayer.config.ts
 import rehypePrettyCode from 'rehype-pretty-code';
+
+import { calculateReadingTime } from '@/lib/content';
 import { defineDocumentType, makeSource } from "contentlayer2/source-files";
+
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
   filePathPattern: `**/*.mdx`,
   contentType: "mdx",
   fields: {
+    tag: { type: "string", required: true },
     title: { type: "string", required: true },
     cover: { type: "string", required: true },
     summary: { type: "string", required: true },
     featured: { type: "boolean", required: true },
     publishedAt: { type: "date", required: true },
-    tags: { type: "list", of: { type: "string" }, required: true },
   },
   computedFields: {
     url: {
       type: "string",
       resolve: (post) => `/blog/${post._raw.flattenedPath}`,
     },
+    readingTime: {
+      type: 'number',
+      resolve: (post) => calculateReadingTime(post.body.raw),
+    }
   },
 }));
 
