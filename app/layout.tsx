e15e372ac0b13a406be71d0b84ac6228b8537ea4
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-27 01:03:24 +0200

    style: change display font to figtree & body to karla

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Figtree, Karla } from "next/font/google";
 
 import "./globals.css";
 import { Header } from "@/components/layout/header";
@@ -7,30 +8,33 @@ import { ThemeProvider } from "@/components/shared/theme-provider";
 import { MaxWidthWrapper } from "@/components/layout/max-width-wrapper";
 
 // --------- FONTS ---------
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
+const fontDisplay = Figtree({
+  variable: "--font-display",
+});
+
+const fontBody = Karla({
+  variable: "--font-body",
+});
+
 // --------- METADATA ---------
 export const metadata: Metadata = {
-  metadataBase: new URL('https://som3aware.vercel.app/'),
+  metadataBase: new URL("https://som3aware.vercel.app/"),
   title: { default: "Som3aware", template: "%s | Som3aware" },
   description: "Crafting elegant products that captivate and delight users ✨",
   openGraph: {
-    title: 'Som3aware',
-    description: 'Crafting elegant products that captivate and delight users ✨',
-    url: 'https://som3aware.vercel.app/',
-    siteName: 'Som3aware',
-    locale: 'en_US',
-    type: 'website',
+    title: "Som3aware",
+    description:
+      "Crafting elegant products that captivate and delight users ✨",
+    url: "https://som3aware.vercel.app/",
+    siteName: "Som3aware",
+    locale: "en_US",
+    type: "website",
   },
   robots: {
     index: true,
@@ -38,14 +42,14 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   twitter: {
-    title: 'Som3aware',
-    card: 'summary_large_image',
+    title: "Som3aware",
+    card: "summary_large_image",
   },
 };
 
@@ -58,7 +62,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} font-sans bg-zinc-50 selection:bg-zinc-200 dark:bg-zinc-900 dark:selection:bg-zinc-700 scroll-smooth antialiased`}
+        className={`${geistMono.variable} ${fontDisplay.variable} ${fontBody.variable} font-body bg-zinc-50 selection:bg-zinc-200 dark:bg-zinc-900 dark:selection:bg-zinc-700 scroll-smooth antialiased`}
       >
         <ThemeProvider
           enableSystem
