57caf9e3c4c7754031dcdb6f6c5e42b33cae11bf
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-26 03:46:11 +0200

    style: remove md:grid-cols-2

--- components/sections/featured-projects.tsx
+++ components/sections/featured-projects.tsx
@@ -8,7 +8,7 @@ export function FeaturedProjects() {
   return (
     <Section>
       <SectionTitle text="Featured projects" />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 gap-4">
         {PROJECTS.filter((p) => p.featured).map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
