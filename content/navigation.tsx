408d08ccec47ef7622d3ed572149a9d0cf19158a
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-26 03:47:05 +0200

    refactor: add blog route

--- content/navigation.tsx
+++ content/navigation.tsx
@@ -3,6 +3,7 @@ import { NavigationItem } from "som3aware";
 const NAVIGATION_ITEMS = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
+  { name: "Blog", href: "/blog" },
   { name: "Projects", href: "/projects" },
 ] satisfies NavigationItem[];
 
