d855eeccf1077518c29c74224a369f5a55282987
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-26 18:30:33 +0200

    refactor: display post date & reading time

--- components/shared/post-card.tsx
+++ components/shared/post-card.tsx
@@ -1,4 +1,6 @@
+import { format } from "date-fns";
 import { ComponentProps } from "react";
+
 import type { Post } from "contentlayer/generated";
 
 // --------- COMPONENT PROPS ---------
@@ -13,10 +15,9 @@ export function PostCard({ post, ...props }: PostCardProps) {
       <div className="rounded-lg border text-card-foreground relative overflow-hidden transition-all duration-300 bg-zinc-100 dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 hover:border-zinc-500 dark:hover:border-zinc-400">
         <div className="flex items-center p-4">
           <div className="flex-grow min-w-0 tracking-tight font-light">
+            <p className="text-sm text-muted-foreground/80">{`${format(post.publishedAt, 'LLL dd, yyyy')} • ${post.readingTime} ${post.readingTime === 1 ? "min" : "mins"} read`}</p>
             <h2 className="font-semibold truncate">{post.title}</h2>
-            <p className="text-sm text-muted-foreground">
-              {post.summary}
-            </p>
+            <p className="text-sm text-muted-foreground">{post.summary}</p>
           </div>
         </div>
       </div>
