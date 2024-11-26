2bc30e34b858e15000e657e48dd5f4fccb057cc6
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-26 18:27:30 +0200

    refactor: remove unused `idx` var

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,7 @@ import { PostCard } from "@/components/shared/post-card";
 import { allPosts, type Post } from "contentlayer/generated";
 import { Section, SectionTitle } from "@/components/shared/section";
 
-
+// --------- PAGE LAYOUT ---------
 export default function BlogPage() {
   const posts: Post[] = allPosts.sort((a, b) =>
     compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))
@@ -13,7 +13,7 @@ export default function BlogPage() {
     <Section>
       <SectionTitle text="Blog posts" />
       <div className="grid grid-cols-1 gap-4">
-        {posts.map((post, idx) => (
+        {posts.map((post) => (
           <PostCard key={post._id} post={post} />
         ))}
       </div>
