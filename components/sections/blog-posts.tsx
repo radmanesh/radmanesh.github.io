de5cacb1d1178088c3cf06f5e3668df29c4cd31d
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-26 03:45:50 +0200

    refactor: complete blog posts for home page

--- components/sections/blog-posts.tsx
+++ components/sections/blog-posts.tsx
@@ -1,10 +1,27 @@
-import { Section, SectionTitle } from "../shared/section";
+import { compareDesc } from "date-fns";
+
+import { allPosts } from "contentlayer/generated";
+import { PostCard } from "@/components/shared/post-card";
+import { AnimatedLink } from "@/components/shared/animated-link";
+import { Section, SectionTitle } from "@/components/shared/section";
 
 export function BlogPosts() {
   return (
     <Section>
       <SectionTitle text="Blog posts" />
-      <div>coming soon...</div>
+      <div className="grid grid-cols-1 gap-4">
+        {allPosts
+          .sort((a, b) =>
+            compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))
+          )
+          .filter((p) => p.featured)
+          .map((post) => (
+            <PostCard post={post} key={post._id} />
+          )).slice(0, 6)}
+      </div>
+      <div className="flex justify-end">
+        <AnimatedLink href="/blog">View all posts</AnimatedLink>
+      </div>
     </Section>
   );
 }
