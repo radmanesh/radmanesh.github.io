8e9402959909a13db835cb9a853749eefe2bc816
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-27 19:30:19 +0200

    chore: remove rehype-pretty-code

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,5 +1,4 @@
 // file: contentlayer.config.ts
-import rehypePrettyCode from "rehype-pretty-code";
 
 import { calculateReadingTime } from "@/lib/content";
 import { defineDocumentType, makeSource } from "contentlayer2/source-files";
@@ -27,26 +26,9 @@ export const Post = defineDocumentType(() => ({
   },
 }));
 
-const prettyCodeOptions = {
-  // Use one of Shiki's packaged themes
-  theme: "rose-pine",
-  // Set to true to keep the background color
-  keepBackground: true,
-  onVisitLine(node: any) {
-    if (node.children.length === 0) {
-      node.children = [{ type: "text", value: " " }];
-    }
-  },
-  onVisitHighlightedLine(node: any) {
-    node.properties.className.push("highlighted");
-  },
-  onVisitHighlightedWord(node: any, id: any) {
-    node.properties.className = ["word"];
-  },
-};
+
 
 export default makeSource({
   contentDirPath: "posts",
   documentTypes: [Post],
-  mdx: { rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]] },
 });
