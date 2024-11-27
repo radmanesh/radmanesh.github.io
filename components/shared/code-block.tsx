9827ee60f3b13ab344420900d965f97e25535bd4
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-27 19:30:54 +0200

    feat: create code block component

---
+++ components/shared/code-block.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Check, Copy } from "lucide-react";
+import { useEffect, useState } from "react";
+import { createHighlighter, BundledLanguage, BundledTheme } from "shiki";
+
+import { Button } from "@/components/ui/button";
+
+// --------- PROPS --------- //
+interface CodeBlockProps {
+  code: string;
+  lang: BundledLanguage;
+}
+
+// --------- COMPONENT --------- //
+export function CodeBlock({ code, lang }: CodeBlockProps) {
+  const { theme } = useTheme();
+  const [html, setHtml] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    const highlight = async () => {
+      const highlighter = await createHighlighter({
+        themes: ["github-light", "github-dark-default"],
+        langs: [lang],
+      });
+
+      const currentTheme = theme === "dark" ? "github-dark-default" : "github-light";
+      const html = highlighter.codeToHtml(code, {
+        lang: lang,
+        theme: currentTheme as BundledTheme,
+      });
+
+      setHtml(html);
+    };
+
+    highlight();
+  }, [code, lang, theme]);
+
+  const copyToClipboard = async () => {
+    await navigator.clipboard.writeText(code);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  return (
+    <div className="relative group rounded-lg">
+      <Button
+        size='icon'
+        variant='outline'
+        onClick={copyToClipboard}
+        className="absolute right-2 top-2 p-2 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity bg-zinc-50 dark:bg-zinc-900 hover:bg-zinc-50/80 hover:dark:bg-zinc-900/80"
+      >
+        {copied ? (
+          <Check className="h-4 w-4 text-green-400" />
+        ) : (
+          <Copy className="h-4 w-4 text-gray-300" />
+        )}
+      </Button>
+      <div className="text-wrap" dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  );
+}
