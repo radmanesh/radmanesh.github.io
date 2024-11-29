cceaff22fc8e016436a4ded1dc8542eaa1991fa0
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-29 20:02:24 +0200

    fix: add `flex-wrap` to classNames

--- components/sections/socials.tsx
+++ components/sections/socials.tsx
@@ -7,9 +7,9 @@ export function Socials() {
   return (
     <Section>
       <SectionTitle text="Connect with me" />
-      <div className="flex items-center gap-2">
+      <div className="flex items-center flex-wrap gap-2">
         {SOCIALS.map(({ href, title }) => (
-          <Link key={title} href={href} className="flex items-center flex-wrap gap-2">
+          <Link key={title} href={href}>
             <span className="text-muted-foreground hover:text-primary font-mono text-sm leading-relaxed tracking-wide font-normal">{title}</span>
           </Link>
         ))}
