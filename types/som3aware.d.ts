905bd342ecab444172080940759395155cbf3378
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-29 19:24:10 +0200

    feat: add education & experience types

--- types/som3aware.d.ts
+++ types/som3aware.d.ts
@@ -8,16 +8,29 @@ declare module "som3aware" {
     description: string;
   };
 
-  export type NavigationItem = {
-    name: string;
-    href: string;
+  export type NavigationItem = { name: string; href: string };
+
+  export type Education = {
+    id: string;
+    period: Period;
+    degree: string;
+    institute: string;
   };
 
-  export type SectionItem = {
-    endDate: Date | 'present';
-    entity:string;
-    image: string;
-    startDate: Date;
-    description: string;
+  export type Experience = {
+    id: string;
+    role: Role;
+    period: Period;
+    company: Company;
   };
+
+  export type Social = {
+    href: string;
+    title: string;
+  };
+
+  // un-exported
+  type Period = { from: string; to: string };
+  type Company = { name: string; url: string };
+  type Role = { title: string; description: string };
 }
