1039a0ad58da480c7cceee5a0db4a506ed1ce1d5
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-29 19:24:41 +0200

    feat: add socials

---
+++ content/socials.tsx
@@ -0,0 +1,12 @@
+import { Social } from "som3aware";
+
+const SOCIALS = [
+  { title: "email", href: "mailto:abokahfa@gmail.com" },
+  { title: "x.com", href: "https://x.com/som3aware" },
+  { title: "github", href: "https://github.com/ahmedsomaa" },
+  { title: "linkedin", href: "https://linkedin.com/in/som3aware" },
+  { title: "hashnode", href: "https://som3aware.hashnode.dev/" },
+  { title: "front-end mentor", href: "https://www.frontendmentor.io/profile/ahmedsomaa" },
+] satisfies Social[];
+
+export default SOCIALS;
