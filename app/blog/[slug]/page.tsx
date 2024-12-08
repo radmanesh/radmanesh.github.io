e462f265aac6083a368ca53acd6726774497834e
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-12-08 19:42:42 +0200

    fix: replace site with siteName

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -36,7 +36,7 @@ export async function generateMetadata({ params }: PostPageProps) {
       description: post.summary,
       url: "https://som3aware.vercel.app/blog/" + slug,
       images: [post.cover],
-      site: "Som3aware",
+      siteName: "Som3aware",
     },
     twitter: {
       title: post.title,
