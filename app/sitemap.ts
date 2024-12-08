a8bb9b1853c3093bb2f34e960b6e12392568f3f0
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-12-08 19:37:18 +0200

    feat: add dynamic sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from "next";
+
+import { allPosts } from "contentlayer/generated";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://som3aware.vercel.app";
+
+  const staticRoutes = ["", "/about", "/blog", "/projects"].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  const blogPostRoutes = allPosts.map((post) => ({
+    url: `${baseUrl}/blog/${post._raw.flattenedPath}`,
+    lastModified: new Date(post.publishedAt).toISOString(),
+  }));
+
+  return [...staticRoutes, ...blogPostRoutes];
+}
